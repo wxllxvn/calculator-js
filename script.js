3adff63c57a729bde0c3ddda6a5d94d80f5e31e1
wxllxvn <willml.contato@gmail.com>
2021-09-25 22:36:45 -0300

    create script

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    var equal = document.getElementById("equal");
+    var cleanAll = document.getElementById("clean-all");
+    var clean = document.getElementById("clean");
+    var result = document.getElementById("result")
+    var buttons = document.getElementsByClassName("buttons");
+
+
+    cleanAll.addEventListener("click", function(){
+        result.innerHTML = "";
+        console.log('ok');
+    })
+
+    clean.addEventListener("click", function(){
+        let newValue = result.innerHTML.split('');
+        newValue.pop();
+        result.innerHTML = newValue.join('');
+        
+    })
+
+    equal.addEventListener("click", function(){
+        result.innerHTML = eval(result.innerHTML);
+    })
+}
